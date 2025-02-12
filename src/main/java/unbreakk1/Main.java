5672ff1571b938a3a222be5ea68d8da402cbdcab
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-12 15:13:18 +0100

    finished basic tasks

--- src/main/java/unbreakk1/Main.java
+++ src/main/java/unbreakk1/Main.java
@@ -2,12 +2,13 @@ package unbreakk1;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> numbers = Arrays.asList(11, 1, 2, 3, 12, 4, 13, 5, 6, 7, 8, 9, 10);
 
         numbers.stream()
                 .filter(number -> number % 2 == 0)
@@ -17,6 +18,23 @@ public class Main
         numbers.stream()
                 .map(number -> number * 2)
                 .forEach(System.out::println);
+
+        // Sort
+        numbers.stream()
+                .sorted()
+                .forEach(System.out::println);
+
+        // Sum
+        int sum = numbers.stream()
+                .reduce(0, Integer::sum);           // (a, b) -> a + b
+        System.out.println("Sum of all numbers: " + sum);
+
+        // to new list
+        List<Integer> doubledNumbers = numbers.stream()
+                .map(number -> number * 2)
+                .collect(Collectors.toList()); // very long for : .toList();
+
+        System.out.println("Doubled Numbers: " + doubledNumbers);
 
     }
 }
