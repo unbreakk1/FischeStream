51e6cd5dd97b1c413547ab538071c55872f20f44
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-12 15:03:53 +0100

    add even and double output

--- src/main/java/unbreakk1/Main.java
+++ src/main/java/unbreakk1/Main.java
@@ -1,9 +1,22 @@
 package unbreakk1;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .forEach(System.out::println);
+
+        // Double
+        numbers.stream()
+                .map(number -> number * 2)
+                .forEach(System.out::println);
 
     }
 }
